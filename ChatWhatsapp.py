5c13d44f85c27457a068b24209794dcf27e08aae
FelipeNascimento7 <felipenascimento02@hotmail.com>
2022-10-18 23:21:55 -0300

    MAIS TESTES

--- ChatWhatsapp.py
+++ ChatWhatsapp.py
@@ -4,28 +4,33 @@ import os
 
 class ChatBotWhatsapp:
 	def __init__(self):
-		with sync_playwright() as p:
+		pass
+	async def inicio(self):
+		async with sync_playwright() as p:
 			navegador = p.chromium.launch(headless=False)
-			pagina = navegador.new_page()
+			context = await navegador.new_context()
+			pagina = await context.new_page()
 			pagina.goto('https://web.whatsapp.com')
+			session_storage = await pagina.evaluate("() => JSON.stringify(sessionStorage)")
+			os.environ["SESSION_STORAGE"] = session_storage
 			time.sleep(10)
+			await context.close()
 
-		session_storage = pagina.evaluate("() => JSON.stringify(sessionStorage)")
-		os.environ["SESSION_STORAGE"] = session_storage
-
+		storage = await context.storage_state(path="state.json")
 
 		session_storage = os.environ["SESSION_STORAGE"]
-		context.add_init_script("""(storage => {
-				  if (window.location.hostname === 'example.com') {
-				    const entries = JSON.parse(storage)
-				    for (const [key, value] of Object.entries(entries)) {
-				      window.sessionStorage.setItem(key, key)
-				    }
-				  }
-				})('""" + session_storage + "')")
+		await context.add_init_script("""(storage => {
+		  if (window.location.hostname === 'example.com') {
+		    const entries = JSON.parse(storage)
+		    for (const [key, value] of Object.entries(entries)) {
+		      window.sessionStorage.setItem(key, key)
+		    }
+		  }
+		})('""" + session_storage + "')")
 
-		with sync_playwright() as p:
+		async with sync_playwright() as p:
 			navegador = p.chromium.launch(headless=False)
+			context = navegador.new_context(storage_state="state.json")
 			pagina = navegador.new_page()
 			pagina.goto('https://web.whatsapp.com')
 			time.sleep(10)
@@ -33,3 +38,4 @@ class ChatBotWhatsapp:
 
 if __name__ == '__main__':
 	chat = ChatBotWhatsapp()
+	chat.inicio()
